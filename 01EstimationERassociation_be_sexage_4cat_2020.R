23d971d10ae545182864b47cb2b6097b2d7c3d0f
huychanchan98 <tdhuy98@gmail.com>
2024-08-01 18:00:10 +0200

    Add files via upload

---
+++ 01EstimationERassociation_be_sexage_4cat_2020.R
@@ -0,0 +1,230 @@
+################################################################################
+# SUPPLEMENTAL MATERIAL of the article:
+#   "A hands-on tutorial on a modelling framework for projections of climate 
+#    change impacts on health"
+#   Ana M. Vicedo-Cabrera, Francesco Sera, Antonio Gasparrini.
+#
+# This code reproduces the analysis described in the tutorial.
+# R Code and data released under GNU General Public License (version 3).
+#
+# [date]
+# * an updated version of this code compatible with future
+#   versions of the software is available at the personal website of the
+#   last author (https://github.com/gasparrini/....)
+#
+# Requirements:
+#  - R version 3.5.1
+#  - 'dlnm' package version 2.3.5
+#  - 'splines' package version  3.5.1
+#  - 'MASS' package version 7.3-50
+#
+################################################################################
+
+# LOAD THE PACKAGES
+library(dlnm) ; library(splines) ; library(MASS)
+
+################################################################################
+# 01 ESTIMATION OF THE EXPOSURE-RESPONSE ASSOCIATIONS
+################################################################################
+
+# Use the observed daily temperature-mortality series to estimate
+#     the coefficients defining the exposure-response association.
+# In this example, we estimate the temperature-related mortality association 
+#     using data from London (UK) between 1990 and 2012.
+# ADAPTATION USE DATA FROM BELGIUM 2010-2015
+
+# LOAD OBSERVED DATA - DAILY TEMPERATURE-SERIES BETWEEN 1990-2012 IN LONDON
+#  NB: file "lndn_obs.csv" provided as downloadable supplemental material  
+#      along this code. Description of the variables available in 
+#     "VarDescr_lndn_obs.csv".
+setwd("D:/Belgium/OneDrive - KU Leuven/KUL/Thesis/code/tutorial/be2020")
+
+obs <- read.csv("bel_obs_summer2020.csv",colClasses=c(date="Date"))
+#obs <- read.csv("bel_obs_male_age_sm2020.csv",colClasses=c(date="Date"))
+#obs <- read.csv("bel_obs_female_age_sm2020.csv",colClasses=c(date="Date"))
+
+# "bel_obs_summer2020.csv" = age group over Belgium summer 2020
+# "bel_obs_male_age_sm2020.csv" = sexage (MALE)  group over Belgium summer 2020
+# "bel_obs_female_age_sm2020.csv" = sexage (FEMALE)  group over Belgium summer 2020
+
+# DEFINITION OF THE CROSS-BASIS FOR TEMPERATURE
+# - SPECIFICATION PARAMETERS OF THE EXPOSURE-RESPONSE DIMENSION OF THE CROSS-BASIS
+
+# argvar: main model, cubic natural spline with three internal knots in 
+#   the 10th, 75th, 90th percentiles of the temperature distribution
+
+argvar <- list(fun="ns", knots = quantile(obs$tmean,c(10,75,90)/100, na.rm=T),
+               Bound=range(obs$tmean,na.rm=T))
+# argavar1 & argvar2: additional tests, argavar1 fits a linear function, and
+#   argvar2 fits a double threshold, with linear relationship for temperatures 
+#   below and above the 10th and 90th percentiles,
+#   with null association in between.
+
+argvar1 <- list(fun="lin")
+argvar2 <- list(fun="thr", thr.value=quantile(obs$tmean,c(10,90)/100, na.rm=T),
+                side="d")
+# - SPECIFICATION PARAMETERS OF THE LAG-ASSOCIATION DIMENSION OF THE CROSS-BASIS
+# Definition of the maximum lag, that is, 7 days
+maxlag <- 7
+# arglag: main model, it fits a cubic natural spline with three internal knots 
+#   equally-spaced in the log-scale.
+arglag <- list(fun="ns",knots=logknots(maxlag,nk=3))
+
+# - CREATE CROSSBASIS OBJECTS
+cb <- crossbasis(obs$tmean,maxlag,argvar,arglag)  
+cb1 <- crossbasis(obs$tmean,maxlag,argvar1,arglag)   
+cb2 <- crossbasis(obs$tmean,maxlag,argvar2,arglag)
+# FIT THE MODEL
+# Include in the model the crossbasis term of temperature, along with the 
+#    indicator for day of day of the week (dow) and natural cubic spline of 
+#    time with 8 df per year.
+
+m <- glm(all ~ cb + dow + ns(date,df=round(8*length(date)/365.25)), 
+         data=obs, family=quasipoisson)
+m1 <- glm(all ~ cb1 + dow + ns(date,df=round(8*length(date)/365.25)), 
+          data=obs, family=quasipoisson)
+m2 <- glm(all ~ cb2 + dow + ns(date,df=round(8*length(date)/365.25)), 
+          data=obs, family=quasipoisson)
+# FIT THE MODEL for different age categories
+# Apply the same model defined in the script 01 to the age-specific counts:
+
+m0_64 <- glm(all_0_64 ~ cb + dow + ns(date,df=round(8*length(date)/365.25)), 
+             data=obs, family=quasipoisson)
+m65_74 <- glm(all_65_74 ~ cb + dow + ns(date,df=round(8*length(date)/365.25)), 
+              data=obs, family=quasipoisson)
+m75_84 <- glm(all_75_84 ~ cb + dow + ns(date,df=round(8*length(date)/365.25)), 
+              data=obs, family=quasipoisson)
+m85plus <- glm(all_85plus ~ cb + dow + ns(date,df=round(8*length(date)/365.25)), 
+               data=obs, family=quasipoisson)
+
+# GET PREDICTIONS & PLOT
+# - DEFINE PROVISIONAL CENTERING POINT TO HAVE THE INITIAL PREDICTION
+#    NB: 'varcen' is initially set to a provisional temperature value within 
+#        the observed range. 
+varcen <- 21 
+
+# - ESTIMATE MMT FROM THE PREDICTED EXPOSURE-RESPONSE ASSOCIATION
+#     FOR EACH AGE CATEGORY
+
+cp0_64 <- crosspred(cb,m0_64,cen=varcen,by=0.1)
+cen0_64 <- cp0_64$predvar[which.min(cp0_64$allRRfit)] 
+
+cp65_74 <- crosspred(cb,m65_74,cen=varcen,by=0.1)
+cen65_74 <- cp65_74$predvar[which.min(cp65_74$allRRfit)] 
+
+cp75_84 <- crosspred(cb,m75_84,cen=varcen,by=0.1)
+cen75_84 <- cp75_84$predvar[which.min(cp75_84$allRRfit)] 
+
+cp85plus <- crosspred(cb,m85plus,cen=varcen,by=0.1)
+cen85plus <- cp85plus$predvar[which.min(cp85plus$allRRfit)] 
+
+# - RE-CENTER & GET PREDICTIONS FOR EACH MODEL CENTERING ON THE MMT 
+pred0_64 <- crosspred(cb, m0_64, cen=cen0_64, by=1)   
+pred65_74 <- crosspred(cb, m65_74, cen=cen65_74, by=1)   
+pred75_84 <- crosspred(cb, m75_84, cen=cen75_84, by=1)  
+pred85plus <- crosspred(cb, m85plus, cen=cen85plus, by=1)  
+
+
+# - ESTIMATE MMT FROM THE PREDICTED EXPOSURE-RESPONSE ASSOCIATION 
+# MMT corresponds to the temperature of minimum mortality, which will be used as
+#    as reference to estimate relative risks and as temperature threshold 
+#    to differentiate the contribution of heat and cold to the total mortality 
+#    attributable to non-optimal temperatures.
+
+cp <- crosspred(cb,m,cen=varcen,by=0.1)
+cen <- cp$predvar[which.min(cp$allRRfit)] 
+
+# - RE-CENTER & GET PREDICTIONS FOR EACH MODEL CENTERING ON THE MMT 
+pred <- crosspred(cb, m, cen=cen, by=1)   
+
+pred1 <- crosspred(cb1, m1, cen=cen, by=0.1)   
+pred2 <- crosspred(cb2, m2, cen=cen, by=0.1)   
+
+################################################################################
+# PLOT - FIGURE 1
+#pdf("Fig1_bel_age_summer2020.pdf",height=4.5,width=10)
+#pdf("Fig1_bel_male_age_sm2020.pdf",height=4.5,width=10)
+#pdf("Fig1_bel_female_age_sm2020.pdf",height=4.5,width=10)
+
+xlab <- expression(paste("Temperature (",degree,"C)"))
+
+layout(matrix(c(1,2,3),ncol=3,byrow=T))
+
+# PLOT - 3D
+par(mar=c(2,3,3,1),mgp=c(3,1,0),las=1,cex.axis=0.9,cex.lab=1)
+plot(pred,"3d",ltheta=150,xlab="Temperature (C)",ylab="Lag",zlab="RR", 
+     col=gray(0.9), main="Exposure-lag-response")
+
+# OVERALL
+# The plots show the cumulative exposure-response association, in terms of 
+#    relative risks (RR) and centered in the MMT, across the 7 days of lag.
+par(mar=c(5,4,3,1),mgp=c(3,1,0),las=1,cex.axis=0.9,cex.lab=1)
+plot(pred,"overall",col="red",ylim=c(0.5,2.5),axes=T,lab=c(6,5,7),xlab=xlab,
+     ylab="RR",main="Overall")
+
+# OVERALL DIFFERENT FUNCTIONS
+# See the different shapes of the exposure-response association using the
+#   three functions (non-linear, linear, double threshold).
+par(mar=c(5,4,3,1),mgp=c(3,1,0),las=1,cex.axis=0.9,cex.lab=1)
+plot(pred,"overall",col="red",ylim=c(0.5,2.5),axes=T,lab=c(6,5,7),xlab=xlab,
+     ylab="RR",ci="n",main="Overall - different functions")
+lines(pred1, col="blue")
+lines(pred2, col="forestgreen")
+
+legend("top",c("Natural spline (Main)","Linear","Double threshold")
+       ,xpd = TRUE,col=c("red","blue","forestgreen"),lwd=2,bg="white"
+       ,cex=0.8,ncol=1,inset=0.04)
+
+layout(1)
+#dev.off()
+
+# NB: run pdf() and dev.off() for saving the plot in pdf format.
+#############################################################################
+#FIGURE 6
+#pdf("Fig6_bel_age_summer2020.pdf",height=5,width=9)
+#pdf("Fig6_bel_male_age_sm2020.pdf",height=5,width=9)
+#pdf("Fig6_bel_female_age_sm2020.pdf",height=5,width=9)
+
+# FIGURE 6A DEMOGRAPHIC CHANGES
+layout(matrix(c(1,2),ncol=2,byrow=T))
+xlab <- expression(paste("Temperature (",degree,"C)"))
+
+par(mar=c(5,4,4,1),mgp=c(3,1,0),las=1,cex.axis=0.9,cex.lab=1)
+plot(pred0_64,"overall",col="magenta",ylim=c(0.5,2.5),axes=T,lab=c(6,5,7),xlab=xlab,
+     ylab="RR",ci="n",main="Demographic changes", lwd=2)
+lines(pred65_74, col="blue", lwd=2, lty=2)
+lines(pred75_84, col="forestgreen", lwd=2, lty=3)
+lines(pred85plus, col="orange", lwd=2, lty=4)
+
+legend(17,2.4,c("<65 years","65-74 years","75-84 years", ">84 years")
+       ,xpd = TRUE,col=c("magenta","blue","forestgreen", "orange"),lwd=2,bg="white"
+       ,cex=0.9,ncol=1,inset=0.04,bty="n", lty=1:4)
+text(21,2.5, "Age-specific relationships", cex=1)
+
+
+# FIGURE 6B ADAPTATION
+
+# INDICATOR FOR HEAT (ABOVE MMT)
+ind <- pred$predvar>=cen 
+par(mar=c(5,4,4,1),mgp=c(3,1,0),las=1,cex.axis=0.9,cex.lab=1)
+
+plot(pred,"overall",col="red",ylim=c(0.5,2.5),axes=T,lab=c(6,5,7),xlab=xlab,
+     ylab="RR",ci="n",main="Adaptation")
+
+lines(pred$predvar,pred$allRRfit,col=2,lwd=2)
+# PLOT LINE OF THE MODIFIED CURVE (30% REDUCTION OF HEAT) 
+lines(pred$predvar[ind],exp(pred$allfit[ind]/1.7),col=2,lwd=2,lty=2)
+legend(17,2.4,c("No adaptation","With adaptation"),lty=1:2,inset=0.005,
+       cex=0.9,bty="n",col=2)
+text(21,2.5, "Reduction in heat risk", cex=1)
+
+layout(1)
+#dev.off()
+
+
+################################################################################
+
+
+
+
+
---
+++ 04_05_06ExtCurveProjUncert_be_countfact_sexage_4cat_sm2020.r
@@ -0,0 +1,347 @@
+################################################################################
+# SUPPLEMENTAL MATERIAL of the article:
+#   "A hands-on tutorial on a modelling framework for projections of climate 
+#    change impacts on health"
+#   Ana M. Vicedo-Cabrera, Francesco Sera, Antonio Gasparrini.
+#
+# This code reproduces the analysis described in the tutorial.
+# R Code and data released under GNU General Public License (version 3).
+#
+# [date]
+# * an updated version of this code compatible with future
+#   versions of the software is available at the personal website of the
+#   last author (https://github.com/gasparrini/....)
+################################################################################
+
+################################################################################
+# 04 EXTRAPOLATION OF THE EXPOSURE-RESPONSE CURVE
+# 05 PROJECTION & QUANTIFICATION OF THE IMPACT
+# 06 ENSEMBLE ESTIMATES & QUANTIFICATION OF THE UNCERTAINTY
+################################################################################
+
+# The three last steps of the analysis (extrapolation of the curve, 
+#   impact projections and quantification of the uncertainty) can be performed 
+#   sequentially using the following code.
+
+# In brief, once we extrapolate the curve, we estimate the daily number of attributable 
+#   deaths (AN) in each scenario, GCM and temperature range. 
+# Then, we compute the sum of ANs per each 10-years period for the ensemble 
+#   per each RCP and temperature range. We also estimate the difference in AN 
+#   relative to the ANs estimated for the current days (2010-19). 
+# By dividing between the total mortality, we estimate the corresponding 
+#   attributable fractions (AFs).
+# Uncertainty of the estimated impacts is expressed in terms of empirical 
+#   confidence intervals, defined as the 2.5th and 97.5th percentiles of the 
+#   empirical distribution of the impacts across coefficients samples and GCMs. 
+#   The distribution is obtained through Monte Carlo simulation of the coefficients.
+
+library(zoo)
+# EXTRACT COEFFICIENTS AND VCOV FROM THE MODEL IN STAGE 1
+# With the crossreduce function we can reduce the fit of the bidimensional DLNM
+#   (of the stage 1 using the observed temperature-mortality series)
+#   to summaries defined in the exposure-response dimension. We can then 
+#   extract the coefficients and the covariance matrix defining the overall 
+#   exposure-response association.
+setwd("D:/Belgium/OneDrive - KU Leuven/KUL/Thesis/code/tutorial/be2020")
+
+# DEFINE PROJECTED MORTALITY SERIES
+# It is computed as the average mortality for each day of the year 
+#   from daily observed deaths, then repeated along the same projection period
+#   of the modelled temperature series.
+
+# Define a function to compute the lagged values
+Lag <- function(x, k) {
+  sapply(k, function(ki) {
+    if (ki > 0) {
+      c(rep(NA, ki), x[1:(length(x) - ki)])
+    } else if (ki < 0) {
+      c(x[(-ki + 1):length(x)], rep(NA, -ki))
+    } else {
+      x
+    }
+  })
+}
+
+deathdoy<- obs$all
+#deathdoy <- tapply(obs$all,as.numeric(format(obs$date,"%j")),
+#                   mean,na.rm=T)[seq(365)] 
+#while(any(isna <- is.na(deathdoy)))
+#  deathdoy[isna] <- rowMeans(Lag(deathdoy,c(-1,1)),na.rm=T)[isna]
+deathproj <- rep(deathdoy,length=nrow(obs)) #Check length=nrow() obs or rcp8p5 !
+
+# STORE THE MORTALITY BY PERIOD
+deathperiod <- sum(deathdoy)*10
+##########################
+
+# Age groups list
+m_category<-list(m0_64 ,m65_74,m75_84,m85plus)
+
+# (2) DIMENSION - RANGE OF TEMPERATURES
+#temprange <- c("tot","cold","heat")
+
+# (3) DIMENSION - ABSOLUTE AN/DIFFERENCE IN AN
+#absrel <- c("abs","rel")
+# (4) DIMENSION - GENERAL CIRCULATION MODELS
+#gcm_be <- c("RMI"="tmean_rmi")
+
+################################################################################
+####### ADAPTATION WITH THE COUNTERFACTUALS CODE ###############################
+# (6) DIMENSION - NUMBER OF ITERATION IN THE MONTE-CARLO SIMULATION 
+nsim <- 1000
+
+# DEFINE THE ARRAY TO STORE RESULTS
+ansim_attr <- array(NA,dim=list(length(m_category),5,nsim+1),
+                    dimnames=list(c("<65y","65-74y","75-84y",">85y"),
+                                  c("obs","mod1","mod2","mod3","mod4"),
+                                  c("est",paste0("sim",seq(nsim)))))
+
+# RUN ACROSS AGE GROUPS
+for (i in seq(m_category)){
+  
+  m_cat=m_category[[i]]
+  red <- crossreduce(cb,m_cat,cen=cen)
+  coef <- coef(red)
+  vcov <- vcov(red)
+
+  # FACTORS TO DEFINE SCENARIOS (FACTUAL + 4 COUNTERFACTUALS)
+  # NEED TO CHECK ADJUST FOR BELGIUM
+  scen_fact <- c(0,2.06,2.65,1.09,2.17) #2.65 (HADCrut5), (-0.1 compare to Swiss)
+  
+    for (g in seq(length(scen_fact))){
+    
+      # DEFINE THE TMEAN (SUBSTRACT FACTOR SCENARIO)
+      tmeanmod <- obs[,"tmean"] - scen_fact[g]
+        
+      # DERIVE THE CENTERED BASIS
+      bvar <- do.call(onebasis,c(list(x=tmeanmod),argvar))
+      cenvec <- do.call(onebasis,c(list(x=cen),argvar))
+      bvarcen <- scale(bvar,center=cenvec,scale=F)
+    
+      # INDICATOR FOR HEAT DAYS
+      indheat <- tmeanmod>cen 
+    
+      # COMPUTE THE DAILY CONTRIBUTIONS OF ATTRIBUTABLE DEATHS
+      an <- (1-exp(-bvarcen%*%coef))*deathproj
+    
+      # SUM 
+      ansim_attr[i,g,"est"] <- sum(an[indheat], na.rm=T) #Check [] number of subscript
+
+      # SAMPLE COEF ASSUMING A MULTIVARIATE NORMAL DISTRIBUTION
+      set.seed(13041975+g)
+      coefsim <- mvrnorm(nsim,coef,vcov)
+    
+    # LOOP ACROSS ITERATIONS
+      for(s in seq(nsim)) {
+      
+      # COMPUTE THE DAILY CONTRIBUTIONS OF ATTRIBUTABLE DEATHS
+        an <- (1-exp(-bvarcen%*%coefsim[s,]))*deathproj
+      
+      # COMPUTE THE RESULTS FOR EACH RANGE AND PERIOD AND SUM
+      # NB: ACCOUNT FOR NO TEMPERATURE BELOW/ABOVE CEN FOR GIVEN PERIODS
+        ansim_attr[i,g,s+1] <- sum(an[indheat], na.rm=T)
+    }
+  }}
+
+
+
+################################################################################
+# SUMMARIZE THE RESULTS 
+# COMPUTE AN/AF (95%CI) IN THE ENSEMBLE, BY RANGE & PERIOD & RCP
+
+# CREATE NEW OBJECTS TO STORE RESULTS
+# We now create 4 new arrays (2 arrays to store the abs and rel AN, and another 2
+#   to store the estimated rel and abs AF) with 4 dimensions each to store the 
+#   ensemble estimates (average impacts across GCMs) with the empirical 
+#   95% confidence intervals. 
+# In this case, the 4 dimensions correspond to the 10-year period, the point estimate 
+#   and the CI, the temperature range and the scenario. 
+
+estci <- c("est","ci.l","ci.u")
+# anabs <- afabs <- anrel <- afrel <- array(NA,dim=c(length(levels(seqperiod)),
+#                                                   length(estci),length(temprange),length(rcp)),
+#                                          dimnames=list(levels(seqperiod),estci,temprange,names(rcp)))
+
+anabs <- afabs <- anabs_mod <- afabs_mod <- array(NA,dim=c(length(m_category),length(estci)),
+                                          dimnames=list(c("<65y","65-74y","75-84y",">85y"),estci))
+
+# ATTRIBUTABLE NUMBERS 
+# ABSOLUTE
+## Factual (Obs data in ansim_attr) 
+anabs[,"est"] <- apply(ansim_attr[,1,],1,mean)
+anabs[,"ci.l"] <- apply(ansim_attr[,1,],1,quantile,0.025)
+anabs[,"ci.u"] <- apply(ansim_attr[,1,],1,quantile,0.975)
+
+## Counterfactual (mean of mod 1 2 3 4 in ansim_attr)
+anabs_mod[,"est"] <- apply(ansim_attr[,2:5,], 1, mean)
+anabs_mod[,"ci.l"] <- apply(ansim_attr[,2:5,], 1, quantile, 0.025)
+anabs_mod[,"ci.u"] <- apply(ansim_attr[,2:5,], 1, quantile, 0.975)
+
+# ATTRIBUTABLE FRACTION
+afabs[,] <- anabs[,]/deathperiod*100
+afabs_mod[,] <- anabs_mod[,]/deathperiod*100
+
+# REMOVE ansim
+#rm(ansim)
+
+################################################################################
+# TEST BAR PLOT #
+# Create the afabs data frame
+# afabs_data <- data.frame(
+#   age_group = c("<65y", "65-74y", "75-84y", ">85y"),
+#   est = afabs[, "est"],
+#   ci.l = afabs[, "ci.l"],
+#   ci.u = afabs[, "ci.u"]
+# )
+# 
+# # Print the data to verify
+# print(afabs)
+# 
+# # Create the barplot
+# barplot <- ggplot(afabs_data, aes(x=age_group, y=est, fill=age_group)) + 
+#   geom_bar(stat="identity", position="dodge", width=0.7) +
+#   geom_errorbar(aes(ymin=ci.l, ymax=ci.u), width=0.2, size=0.7) +
+#   scale_fill_manual(values = c("red", "orange", "green", "blue"),
+#                     labels=c("<65y", "65-74y", "75-84y", ">85y")) +
+#   labs(x = "Age Group", y = "Heat-related Mortality (%)",
+#        title = "Heat-related Mortality by Age Group") +
+#   theme_minimal() +
+#   theme(legend.position="bottom",
+#         legend.title = element_blank(),
+#         legend.spacing.x= unit(0.3, 'cm'),
+#         legend.text = element_text(size=11),
+#         plot.margin = unit(c(2, 1, 1, 0), units = "lines"),
+#         axis.text.y = element_text(size=10), 
+#         axis.title.y = element_text(size=12, vjust = 0.5),
+#         panel.grid.major.y = element_line(linewidth=1),
+#         panel.grid.major.x = element_blank(),
+#         panel.grid.minor = element_blank())
+# 
+# # Save the plot
+# #ggsave("barplot_age_group.pdf", plot = barplot, width=10, height=6)
+# 
+# # Display the plot
+# print(barplot)
+
+#----------------------------- BARPLOT -----------------------------------------
+# Create afabs_data data frame
+afabs_data <- data.frame(
+  age_group = factor(c("<65y", "65-74y", "75-84y", ">85y"), levels = c("<65y", "65-74y", "75-84y", ">85y")),
+  est = afabs[, "est"],
+  ci.l = afabs[, "ci.l"],
+  ci.u = afabs[, "ci.u"]
+)
+
+# Create obs_abs_f_data data frame
+afabs_mod_data <- data.frame(
+  age_group = factor(c("<65y", "65-74y", "75-84y", ">85y"), levels = c("<65y", "65-74y", "75-84y", ">85y")),
+  est = c(afabs_mod[,"est"]),
+  ci.l = c(afabs_mod[,"ci.l"]),
+  ci.u = c(afabs_mod[,"ci.u"])
+)
+# Add a column to identify the dataset
+afabs_data$dataset <- "afabs"
+afabs_mod_data$dataset <- "afabs_mod"
+
+# Combine both datasets
+combined_data <- rbind(afabs_data, afabs_mod_data)
+
+# Create the barplot
+barplot <- ggplot(combined_data, aes(x=age_group, y=est, fill=dataset)) + 
+  geom_bar(stat="identity", position=position_dodge(width=0.7), width=0.6) +
+  geom_errorbar(aes(ymin=ci.l, ymax=ci.u), width=0.2, position=position_dodge(width=0.7), size=0.7) +
+  scale_fill_manual(values = c("red","blue"),
+                    labels=c("Factual","Counterfactual")) +
+  labs(x = "Age Group", y = "Heat-related Mortality (%)",
+       title = "Heat-related Mortality by Age Group") +
+  theme_minimal() +
+  theme(legend.position="bottom",
+        legend.title = element_blank(),
+        legend.spacing.x= unit(0.3, 'cm'),
+        legend.text = element_text(size=11),
+        plot.margin = unit(c(2, 1, 1, 0), units = "lines"),
+        axis.text.y = element_text(size=10), 
+        axis.title.y = element_text(size=12, vjust = 0.5),
+        panel.grid.major.y = element_line(linewidth=1),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor = element_blank())
+
+# Display the plot
+print(barplot)
+# Save the plot
+#ggsave("Heatmort_bel_age_sm2020.pdf", plot = barplot, width=10, height=6)
+#ggsave("Heatmort_bel_male_age_sm2020.pdf", plot = barplot, width=10, height=6)
+#ggsave("Heatmort_bel_female_age_sm2020.pdf", plot = barplot, width=10, height=6)
+
+
+
+#####################################################
+# FIGURE DENSITY BETWEEN OBS AND COUNTERFACTUAL SCENARIOS
+library(tidyverse)
+#pdf("Density_bel_summer2020.pdf",height=5,width=9)
+
+# Initialize necessary variables
+scen_fact <- c(0, 2.06, 2.65, 1.09, 2.17) # Factor scenarios
+
+# Initialize a list to store data frames for each scenario
+all_data_long <- list()
+
+for (i in seq(m_category)) {
+  
+  m_cat = m_category[[i]]
+  red <- crossreduce(cb, m_cat, cen = cen)
+  coef <- coef(red)
+  vcov <- vcov(red)
+  
+  for (g in seq(length(scen_fact))) {
+    
+    # Define the modified temperature (subtract scenario factor)
+    tmeanmod <- obs$tmean - scen_fact[g]
+    
+    # Prepare the data frame
+    data <- data.frame(
+      obs_tmean = obs$tmean,
+      tmeanmod = tmeanmod,
+      scenario = paste("Scenario", scen_fact[g])
+    )
+    
+    # Reshape the data using pivot_longer and update Type names
+    data_long <- data %>%
+      pivot_longer(cols = c(obs_tmean, tmeanmod), names_to = "Type", values_to = "Temperature") %>%
+      mutate(Type = ifelse(Type == "obs_tmean", "Observed", scenario))
+    
+    # Append to the list
+    all_data_long[[g]] <- data_long
+  }
+}
+
+# Combine all the data frames into one
+combined_data_long <- bind_rows(all_data_long)
+
+# Ensure 'Observed' is plotted last
+combined_data_long$Type <- factor(combined_data_long$Type, levels = c("Scenario 0", "Scenario 2.06", "Scenario 2.65", "Scenario 1.09", "Scenario 2.17", "Observed"))
+
+# Define colors for the plots with increasing opacity and lighter shades of yellow
+colors <- c("Observed" = "red", 
+            "Scenario 0" = "yellow", 
+            "Scenario 2.06" = "lightgoldenrodyellow", 
+            "Scenario 2.65" = "khaki", 
+            "Scenario 1.09" = "gold", 
+            "Scenario 2.17" = "orange")
+
+# Create the density plot
+ggplot(combined_data_long, aes(x = Temperature, color = Type, fill = Type)) +
+  geom_density(alpha = 0.5, size = 1, color = "white") +
+  scale_color_manual(values = colors) +
+  scale_fill_manual(values = colors) +
+  labs(title = "Observed vs. Counterfactual Temperature",
+       x = "Mean Temperature (°C)",
+       y = "Density") +
+  theme_minimal() +
+  theme(legend.position = "bottom",
+  plot.title = element_text(hjust = 0.5) # Center the title
+) +
+  scale_x_continuous(limits = c(5, 40), breaks = seq(5, 40, by = 5)) # Set x-axis limits and breaks
+
+#dev.off()
+# NB: uncomment pdf() and dev.off() for saving the plot in pdf format.
+
